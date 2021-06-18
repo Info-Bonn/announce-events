a404054eee9df6f98f928607ed16820929610916
nonchris <git@chris-ge.de>
2021-06-18 14:31:56 +0200

    Add function specific env variables

--- src/environment.py
+++ src/environment.py
@@ -31,3 +31,8 @@ PREFIX = load_env("PREFIX", "b!")
 VERSION = load_env("VERSION", "unknown")  # version of the bot
 OWNER_NAME = load_env("OWNER_NAME", "unknown")   # owner name with tag e.g. pi#3141
 OWNER_ID = int(load_env("OWNER_ID", "100000000000000000"))  # discord id of the owner
+
+# defaults are set for Info Bonn Server
+EVENT_ROLE = int(load_env("EVENT_ROLE", "855395051046436885"))  # id of the role to be pinged in event announcement
+EVENT_CHANNEL = int(load_env("EVENT_CHANNEL", "760490896298082314"))  # id of channel to send announcement into
+ROLE_EVENT_HOST = int(load_env("ROLE_EVENT_HOST", "855397441784250368"))  # role of event hosts for permission check
